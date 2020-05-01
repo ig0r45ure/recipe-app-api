d76dc99899ace731f160cde0a1e573759a326491
ig0r45ure <igor4sure@gmail.com>
2020-05-01 18:52:31 +0200

    Finished custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -7,14 +7,26 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user with password as a username"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("User must have an email address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user(save=self._db)
+        user.save(using=self._db)
 
         return user
 
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custm user model that supports using email instead of username"""
+    """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
